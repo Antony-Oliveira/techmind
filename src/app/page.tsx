a325ba464b8ca4d9ede9e90773a75ce0028e9d00
Antony <antony.caiquec@gmail.com>
2024-04-10 10:58:16 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
       <section className="bg-ct-blue-600 min-h-screen pt-20">
         <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
           <p className="text-3xl font-semibold">
-            Nextjs 14 + Prisma ORM + shadCN
+            Nextjs 14 + Prisma ORM + NextUI
           </p>
         </div>
       </section>
